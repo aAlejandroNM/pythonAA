e6d05a35da7686b74ac385f467307138d4227e76
JuanFelipeSolanoAnteliz <juanfesolant0906@gmail.com>
2024-04-29 21:49:03 -0500

    Feat se agrego ejercicio listas

--- apuntespython.py
+++ apuntespython.py
@@ -151,4 +151,20 @@
 #ejercicio Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una 
 #lista, pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> 
 #donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas por el usuario.
-todas_materias = ['matematicas', 'fisica','quimica','historia']
+# todas_materias = ['matematicas', 'fisica','quimica','historia']
+# notas = []
+# for i in range(0,len(todas_materias)):
+#     print("cuanto ha sacado en: ", todas_materias[i])
+    
+#     nota = float(int(input()))
+#     notas.append(nota)
+
+# for i in range(0,len(todas_materias)):
+#     print(f"En {todas_materias[i]} saco {notas[i]}")
+
+# Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas.
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+print(reverse(numbers))
+    
+    
